49d72dd17b0d24cb61daea4bc35d1448800ea821
calvinaja2004-svg <calvinaja2004@gmail.com>
2026-02-12 13:50:49 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,1 +1,16 @@
+<?php
+session_start(); // Pindah ke atas
 
+$host = 'localhost';
+$dbname = 'tanyakampus';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch(PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
+?>
